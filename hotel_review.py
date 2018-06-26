8f14da4b556d930d9ba5e53445025b7d6f30523b
Rushikesh Pupale <pupalerushikesh@gmail.com>
2018-06-26 23:28:07 +0530

    Added Model-2

--- hotel_review.py
+++ hotel_review.py
@@ -26,6 +26,9 @@ y = df.iloc[:, 3]
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
+#########---------------------------------##########################
+########################Model-1#########################################
+
 #Buildign a word count vector 
 from sklearn.feature_extraction.text import CountVectorizer
 
@@ -33,7 +36,8 @@ vect = CountVectorizer()
 
 
 X_train_vectorized = vect.fit_transform(X_train['Review_Text'])
-a = X_train_vectorized.toarray()
+
+
 
 
 #Applying Logistic Regreesion
@@ -43,18 +47,53 @@ model = LogisticRegression()
 model.fit(X_train_vectorized, y_train)
 
 
-from sklearn.metrics import roc_auc_score
+
+
+
 
 predictions = model.predict(vect.transform(X_test['Review_Text']))
 
-print('AUC: ', roc_auc_score(y_test, predictions))
+
+
 
 
 from sklearn.metrics import confusion_matrix
 
 cm = confusion_matrix(y_test, predictions)
+
 #got 0.80 accuracy evaluated using confusion matrix
 
 
+feature_names = np.array(vect.get_feature_names())
+sorted_coef_index = model.coef_[0].argsort()
+#printing most negative and positive words
+print('Smallest Coefs: \n{} \n'.format(feature_names[sorted_coef_index[:10]]))
+print('Largest Coefs: \n{} \n'.format(feature_names[sorted_coef_index[:-11:-1]]))
 
+
+###########------------------------###############################
+#######################Model-2##########################################
+
+#Tf-idf term weighting 
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+#min_df = 5 will remove words from vocabulary that appears in less than five doc
+vect = TfidfVectorizer(min_df=5).fit(X_train['Review_Text'])
+len(vect.get_feature_names())
+#reduced features to 1808 from 5619
+
+X_train_vectorized = vect.transform(X_train['Review_Text'])
+
+model = LogisticRegression()
+model.fit(X_train_vectorized, y_train)
+predictions = model.predict(vect.transform(X_test['Review_Text']))
+cm = confusion_matrix(y_test, predictions)
+#Though we reduced the features from 5619 to 1808 the accuracy reduced to 0.78 from 0.80
+
+
+feature_names = np.array(vect.get_feature_names())
+
+sorted_tfidf_index = X_train_vectorized.max(0).toarray()[0].argsort()
+print('Smallest Coefs: \n{} \n'.format(feature_names[sorted_tfidf_index[:10]]))
+print('Largest Coefs: \n{} \n'.format(feature_names[sorted_tfidf_index[:-11:-1]]))
 
